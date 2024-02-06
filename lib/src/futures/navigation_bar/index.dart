176c5d2398fe556face0e73dbd1db1d1f7b30204
Muhammad Rhomaedi <rhomaedycreator@gmail.com>
2024-02-06 17:41:13 +0700

    master: add flutter toast

--- lib/src/futures/navigation_bar/index.dart
+++ lib/src/futures/navigation_bar/index.dart
@@ -4,7 +4,9 @@ import 'package:atendence_hcs/src/futures/profile/views/index.dart';
 import 'package:atendence_hcs/src/futures/slip_gaji/views/index.dart';
 import 'package:atendence_hcs/utils/components/colors.dart';
 import 'package:community_material_icon/community_material_icon.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
 
 class BottomNavigationBarHome extends StatefulWidget {
@@ -35,59 +37,76 @@ class _BottomNavigationBarHomeState extends State<BottomNavigationBarHome> {
     });
   }
 
-  Future<bool> _onPop() async {
-    return false;
+  DateTime timeBackPresed = DateTime.now();
+
+  Future<bool> onPop() async {
+    final diference = DateTime.now().difference(timeBackPresed);
+    final isExitWaring = diference >= const Duration(seconds: 2);
+
+    timeBackPresed = DateTime.now();
+
+    if (isExitWaring) {
+      Fluttertoast.showToast(
+        msg: "Tekan Lagi untuk Keluar",
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1,
+        backgroundColor: const Color(0x8A272727),
+        textColor: Colors.black,
+      );
+      return false;
+    } else {
+      Fluttertoast.cancel();
+      return true;
+    }
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: WillPopScope(
-        onWillPop: () => _onPop(),
-        child: Center(
-          child: _widgetOptions.elementAt(_selectedIndex),
+  Widget build(BuildContext context) => WillPopScope(
+        onWillPop: onPop,
+        child: Scaffold(
+          body: Center(
+            child: _widgetOptions.elementAt(_selectedIndex),
+          ),
+          bottomNavigationBar: Container(
+            decoration: const BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: cGrey_300,
+                  blurRadius: 5,
+                  offset: Offset(0, 0), // Shadow position
+                ),
+              ],
+            ),
+            child: BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              backgroundColor: Colors.white,
+              items: const <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: Icon(CommunityMaterialIcons.home),
+                  label: 'Home',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(CommunityMaterialIcons.alarm),
+                  label: 'Jadwal',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.calendar_month_outlined),
+                  label: 'Permohonan',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(CommunityMaterialIcons.file_document_outline),
+                  label: 'Slip Gaji',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(CommunityMaterialIcons.account_outline, size: 25),
+                  label: 'Profile',
+                ),
+              ],
+              currentIndex: _selectedIndex,
+              selectedItemColor: cPrimary,
+              onTap: _onItemTapped,
+            ),
+          ),
         ),
-      ),
-      bottomNavigationBar: Container(
-        decoration: const BoxDecoration(
-          boxShadow: [
-            BoxShadow(
-              color: cGrey_300,
-              blurRadius: 5,
-              offset: Offset(0, 0), // Shadow position
-            ),
-          ],
-        ),
-        child: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          backgroundColor: Colors.white,
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(CommunityMaterialIcons.home),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(CommunityMaterialIcons.alarm),
-              label: 'Jadwal',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.calendar_month_outlined),
-              label: 'Permohonan',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(CommunityMaterialIcons.file_document_outline),
-              label: 'Slip Gaji',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(CommunityMaterialIcons.account_outline, size: 25),
-              label: 'Profile',
-            ),
-          ],
-          currentIndex: _selectedIndex,
-          selectedItemColor: cPrimary,
-          onTap: _onItemTapped,
-        ),
-      ),
-    );
-  }
+      );
 }
