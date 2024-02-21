abe0d7551b0bb11eba32ecee0777767648ca6739
Muhammad Rhomaedi <rhomaedycreator@gmail.com>
2024-02-21 15:29:03 +0700

    master: add filter histori surat peringatan

--- lib/src/futures/SDM/histori_surat_peringatan/views/index.dart
+++ lib/src/futures/SDM/histori_surat_peringatan/views/index.dart
@@ -1,8 +1,11 @@
+import 'package:atendence_hcs/routes/route_name.dart';
 import 'package:atendence_hcs/src/futures/SDM/components/empty_data.dart';
+import 'package:atendence_hcs/utils/components/all_widget.dart';
 import 'package:atendence_hcs/utils/components/colors.dart';
 import 'package:atendence_hcs/utils/components/my_appbar.dart';
 import 'package:atendence_hcs/utils/components/my_border.dart';
 import 'package:atendence_hcs/utils/components/space.dart';
+import 'package:community_material_icon/community_material_icon.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -42,6 +45,43 @@ class _HistoriSuratPeringatanState extends State<HistoriSuratPeringatan> {
                 child: Column(
                   children: [
                     selectKategori(),
+                    searchKaryawan(),
+                    spaceHeight(10),
+                    Container(
+                      height: 40,
+                      decoration: const BoxDecoration(
+                        color: cPrimary,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(5),
+                        ),
+                      ),
+                      width: Get.width,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: cPrimary,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Icon(
+                              CommunityMaterialIcons.filter_outline,
+                              color: Colors.white,
+                              size: 20,
+                            ),
+                            spaceWidth(5),
+                            const Text(
+                              "Tampilkan",
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
                   ],
                 ),
               ),
@@ -98,6 +138,74 @@ class _HistoriSuratPeringatanState extends State<HistoriSuratPeringatan> {
           );
         }).toList(),
       ),
+    );
+  }
+
+  Widget searchKaryawan() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Karyawan",
+          style: customTextStyle(
+            FontWeight.w600,
+            12,
+            cGrey_900,
+          ),
+        ),
+        spaceHeight(4),
+        InkWell(
+          onTap: () {
+            Get.toNamed(
+              RouteNames.searchKaryawan,
+              arguments: RouteNames.historiSuratPeringatan,
+            );
+          },
+          child: Container(
+            width: Get.width,
+            height: 45,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: const BorderRadius.all(
+                Radius.circular(5),
+              ),
+              border: Border.all(color: cGrey_600, width: 1),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Expanded(
+                    flex: 1,
+                    child: Icon(
+                      Icons.search,
+                      color: cGrey_900,
+                      size: 22,
+                    ),
+                  ),
+                  spaceWidth(8),
+                  Expanded(
+                    flex: 10,
+                    child: Text(
+                      nip == "" ? nama : "$nip - $nama",
+                      style: const TextStyle(
+                        color: cGrey_900,
+                        fontSize: 15,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ),
+                  const Expanded(
+                    flex: 1,
+                    child: SizedBox(),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
