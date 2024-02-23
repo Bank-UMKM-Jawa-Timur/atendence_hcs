aeecfc82c09a32cb7904daa74518f8b093fc34fb
sahrullahh <49519441+sahrullahh@users.noreply.github.com>
2024-02-23 14:04:45 +0700

    design-laporan: add design mutasi and demosi page

--- lib/src/futures/SDM/laporan_pergerakan_karir/views/laporan_demosi.dart
+++ lib/src/futures/SDM/laporan_pergerakan_karir/views/laporan_demosi.dart
@@ -1,13 +1,272 @@
 import 'package:atendence_hcs/utils/components/my_appbar.dart';
+import 'package:atendence_hcs/utils/components/colors.dart';
+import 'package:atendence_hcs/src/futures/SDM/components/empty_data.dart';
+import 'package:atendence_hcs/utils/components/all_widget.dart';
+import 'package:atendence_hcs/utils/components/my_datepicker.dart';
+import 'package:atendence_hcs/utils/components/space.dart';
+import 'package:community_material_icon/community_material_icon.dart';
+import 'package:atendence_hcs/utils/constant.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class LaporanDemosi extends StatelessWidget {
+class LaporanDemosi extends StatefulWidget {
   const LaporanDemosi({super.key});
 
   @override
+  State<LaporanDemosi> createState() => _LaporanDemosiState();
+}
+
+class _LaporanDemosiState extends State<LaporanDemosi> {
+  DateTime firstDate = DateTime.now();
+  DateTime lastDate = DateTime.now();
+  DateTime year = DateTime.now();
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: appBarPrimary("LaporanDemosi"),
+        backgroundColor: cGrey_200,
+        appBar: appBarPrimary("Laporan Demosi"),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+          child: Column(children: [
+            Container(
+              width: Get.width,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(7),
+                ),
+                border: Border.all(color: cGrey_400, width: 1),
+              ),
+              child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  child: Column(children: [
+                    formFirstDate(context),
+                    spaceHeight(15),
+                    formLastDate(context),
+                    spaceHeight(15),
+                    Container(
+                      height: 40,
+                      decoration: const BoxDecoration(
+                        color: cPrimary,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(5),
+                        ),
+                      ),
+                      width: Get.width,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          setState(() {});
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: cPrimary,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Icon(
+                              CommunityMaterialIcons.filter_outline,
+                              color: Colors.white,
+                              size: 20,
+                            ),
+                            spaceWidth(5),
+                            const Text(
+                              "Tampilkan",
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ])),
+            ),
+            spaceHeight(10),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Container(
+                  width: Get.width,
+                  height: Get.height / 2,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(color: cGrey_400, width: 1),
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(6),
+                    ),
+                  ),
+                  child: emptyDataSetTitle(
+                    "Silahkan Filter untuk menampilkan data\nHistori Surat Peringatan.",
+                  ),
+                ),
+              ),
+            )
+          ]),
+        ));
+  }
+
+  Column formFirstDate(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        spaceHeight(7),
+        Text(
+          "Dari",
+          style: customTextStyle(
+            FontWeight.w600,
+            12,
+            cGrey_900,
+          ),
+        ),
+        spaceHeight(4),
+        InkWell(
+          onTap: () async {
+            if (GetPlatform.isAndroid) {
+              final date = await datePicker(context, DateTime.now());
+              if (mounted) {
+                setState(() {
+                  firstDate = date ?? DateTime.now();
+                });
+              }
+            } else if (GetPlatform.isIOS) {
+              showCupertinoModalPopup(
+                context: context,
+                builder: (_) => Container(
+                  height: 400,
+                  color: const Color.fromARGB(255, 255, 255, 255),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 300,
+                        child: CupertinoDatePicker(
+                            initialDateTime: DateTime.now(),
+                            maximumDate: DateTime.now(),
+                            maximumYear: DateTime.now().year,
+                            minimumYear: 2023,
+                            mode: CupertinoDatePickerMode.date,
+                            onDateTimeChanged: (val) {
+                              setState(() {
+                                firstDate = val;
+                              });
+                            }),
+                      ),
+                      // Close the modal
+                      CupertinoButton(
+                        child: const Text('OK'),
+                        onPressed: () => Navigator.of(context).pop(),
+                      )
+                    ],
+                  ),
+                ),
+              );
+            }
+          },
+          child: Container(
+            width: Get.width,
+            height: 45,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: const BorderRadius.all(
+                Radius.circular(5),
+              ),
+              border: Border.all(color: cGrey_600, width: 1),
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(firstDate.simpleDate().toString()),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Column formLastDate(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        spaceHeight(7),
+        Text(
+          "Sampai",
+          style: customTextStyle(
+            FontWeight.w600,
+            12,
+            cGrey_900,
+          ),
+        ),
+        spaceHeight(4),
+        InkWell(
+          onTap: () async {
+            if (GetPlatform.isAndroid) {
+              final date = await datePicker(context, DateTime.now());
+              if (mounted) {
+                setState(() {
+                  lastDate = date ?? DateTime.now();
+                });
+              }
+            } else if (GetPlatform.isIOS) {
+              showCupertinoModalPopup(
+                context: context,
+                builder: (_) => Container(
+                  height: 400,
+                  color: const Color.fromARGB(255, 255, 255, 255),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 300,
+                        child: CupertinoDatePicker(
+                            initialDateTime: DateTime.now(),
+                            maximumDate: DateTime.now(),
+                            maximumYear: DateTime.now().year,
+                            minimumYear: 2023,
+                            mode: CupertinoDatePickerMode.date,
+                            onDateTimeChanged: (val) {
+                              setState(() {
+                                lastDate = val;
+                              });
+                            }),
+                      ),
+                      // Close the modal
+                      CupertinoButton(
+                        child: const Text('OK'),
+                        onPressed: () => Navigator.of(context).pop(),
+                      )
+                    ],
+                  ),
+                ),
+              );
+            }
+          },
+          child: Container(
+            width: Get.width,
+            height: 45,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: const BorderRadius.all(
+                Radius.circular(5),
+              ),
+              border: Border.all(color: cGrey_600, width: 1),
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(lastDate.simpleDate().toString()),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
