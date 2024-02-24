10d6fe14ce39303d060971bd32e68ff8e0191f01
sahrullahh <49519441+sahrullahh@users.noreply.github.com>
2024-02-24 14:45:34 +0700

    design-laporan: add laporan page data jamsostek

--- lib/src/futures/SDM/laporan_data_jamsostek/views/index.dart
+++ lib/src/futures/SDM/laporan_data_jamsostek/views/index.dart
@@ -1,13 +1,362 @@
 import 'package:atendence_hcs/utils/components/my_appbar.dart';
+import 'package:atendence_hcs/utils/components/colors.dart';
+import 'package:atendence_hcs/utils/components/all_widget.dart';
+import 'package:atendence_hcs/utils/components/space.dart';
+import 'package:atendence_hcs/src/futures/SDM/components/empty_data.dart';
+import 'package:atendence_hcs/utils/components/my_border.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class LaporanDataJamsostek extends StatelessWidget {
+class LaporanDataJamsostek extends StatefulWidget {
   const LaporanDataJamsostek({super.key});
+
+  @override
+  State<LaporanDataJamsostek> createState() => _LaporanDataJamsostekState();
+}
+
+class _LaporanDataJamsostekState extends State<LaporanDataJamsostek> {
+  String dropdownValue = "Rekap Keseluruhan";
+  String dropdownValueYears = "2024";
+  String dropdownValueMonth = "Januari";
+  String dropdownValueKantor = "Surabaya";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: appBarPrimary("LaporanDataJamsostek"),
+      backgroundColor: cGrey_200,
+      appBar: appBarPrimary("Laporan data Jamsostek"),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+        child: Column(
+          children: [
+            Container(
+              width: Get.width,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(7),
+                ),
+                border: Border.all(color: cGrey_400, width: 1),
+              ),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                child: Column(children: [
+                  formSelectKategori(),
+                  dropdownValue == "Rekap Kantor Cabang"
+                      ? formSelectKantor()
+                      : Container(),
+                  dropdownValue == "Cabang" ? formSelectCabang() : Container(),
+                  formSelectYears(),
+                  formSelectMonth(),
+                ]),
+              ),
+            ),
+            spaceHeight(10),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Container(
+                  width: Get.width,
+                  height: Get.height / 2,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(color: cGrey_400, width: 1),
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(6),
+                    ),
+                  ),
+                  child: emptyDataSetTitle(
+                    "Silahkan Filter untuk menampilkan data\nLaporan data Jamsostek.",
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget formSelectKategori() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text("Kategori",
+            style: customTextStyle(
+              FontWeight.w600,
+              12,
+              cGrey_700,
+            )),
+        spaceHeight(4),
+        Container(
+          width: Get.width,
+          height: 60,
+          decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(
+                Radius.circular(7),
+              )),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 7),
+            child: DropdownButtonFormField<String>(
+              hint: Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Text(dropdownValue),
+              ),
+              isDense: true,
+              isExpanded: true,
+              value: dropdownValue,
+              decoration: const InputDecoration(
+                contentPadding: EdgeInsets.all(8),
+                focusedBorder: focusedBorder,
+                enabledBorder: enabledBorder,
+                border: OutlineInputBorder(),
+                filled: true,
+                fillColor: Colors.white,
+              ),
+              onChanged: (String? newValue) {
+                setState(() {
+                  dropdownValue = newValue!;
+                  // historiSpC.kategori.value = newValue;
+                });
+              },
+              items: <String>['Rekap Keseluruhan', 'Rekap Kantor Cabang']
+                  .map<DropdownMenuItem<String>>((String values) {
+                return DropdownMenuItem<String>(
+                  value: values,
+                  child: Text(values),
+                );
+              }).toList(),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget formSelectKantor() {
+    dropdownValue = "Pusat";
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text("Kantor",
+            style: customTextStyle(
+              FontWeight.w600,
+              12,
+              cGrey_700,
+            )),
+        spaceHeight(4),
+        Container(
+          width: Get.width,
+          height: 60,
+          decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(
+                Radius.circular(7),
+              )),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 7),
+            child: DropdownButtonFormField<String>(
+              hint: Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Text(dropdownValue),
+              ),
+              isDense: true,
+              isExpanded: true,
+              value: dropdownValue,
+              decoration: const InputDecoration(
+                contentPadding: EdgeInsets.all(8),
+                focusedBorder: focusedBorder,
+                enabledBorder: enabledBorder,
+                border: OutlineInputBorder(),
+                filled: true,
+                fillColor: Colors.white,
+              ),
+              onChanged: (String? newValue) {
+                setState(() {
+                  dropdownValue = newValue!;
+
+                  // historiSpC.kategori.value = newValue;
+                });
+              },
+              items: <String>['Pusat', 'Cabang']
+                  .map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget formSelectYears() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Tahun",
+          style: customTextStyle(
+            FontWeight.w600,
+            12,
+            cGrey_700,
+          ),
+        ),
+        spaceHeight(4),
+        Container(
+          width: Get.width,
+          height: 65,
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(
+              Radius.circular(5),
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 2),
+            child: DropdownButtonFormField<String>(
+              hint: Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Text(dropdownValueYears),
+              ),
+              isDense: true,
+              isExpanded: true,
+              value: dropdownValueYears,
+              decoration: const InputDecoration(
+                contentPadding: EdgeInsets.all(8),
+                focusedBorder: focusedBorder,
+                enabledBorder: enabledBorder,
+                border: OutlineInputBorder(),
+                filled: true,
+                fillColor: Colors.white,
+              ),
+              onChanged: (String? newValue) {
+                setState(() {
+                  dropdownValueYears = newValue!;
+                  // historiSpC.kategori.value = newValue;
+                });
+              },
+              items: <String>['2024', '2023', '2022']
+                  .map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget formSelectMonth() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text("Bulan",
+            style: customTextStyle(
+              FontWeight.w600,
+              12,
+              cGrey_700,
+            )),
+        spaceHeight(4),
+        Container(
+          width: Get.width,
+          height: 65,
+          decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(
+                Radius.circular(5),
+              )),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 2),
+            child: DropdownButtonFormField<String>(
+              hint: Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Text(dropdownValueMonth),
+              ),
+              isDense: true,
+              isExpanded: true,
+              value: dropdownValueMonth,
+              decoration: const InputDecoration(
+                contentPadding: EdgeInsets.all(8),
+                focusedBorder: focusedBorder,
+                enabledBorder: enabledBorder,
+                border: OutlineInputBorder(),
+                filled: true,
+                fillColor: Colors.white,
+              ),
+              onChanged: (String? newValue) {
+                setState(() {
+                  dropdownValueMonth = newValue!;
+                  // historiSpC.kategori.value = newValue;
+                });
+              },
+              items: <String>[
+                'Januari',
+                'Februari',
+                'Maret',
+                'April',
+                'Mei',
+                'Juni',
+                'Juli',
+                'Agustus',
+                'September',
+                'Oktober',
+                'November',
+                'Desember'
+              ].map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget formSelectCabang() {
+    dropdownValueKantor = "Surabaya";
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 7),
+      child: DropdownButtonFormField<String>(
+        hint: Padding(
+          padding: const EdgeInsets.only(left: 10),
+          child: Text(dropdownValueKantor),
+        ),
+        isDense: true,
+        isExpanded: true,
+        value: dropdownValueKantor,
+        decoration: const InputDecoration(
+          contentPadding: EdgeInsets.all(8),
+          focusedBorder: focusedBorder,
+          enabledBorder: enabledBorder,
+          border: OutlineInputBorder(),
+          filled: true,
+          fillColor: Colors.white,
+        ),
+        onChanged: (String? newValue) {
+          setState(() {
+            dropdownValueKantor = newValue!;
+            // historiSpC.kategori.value = newValue;
+          });
+        },
+        items: <String>['Surabaya', 'Bondowoso', 'Ponorogo']
+            .map<DropdownMenuItem<String>>((String value) {
+          return DropdownMenuItem<String>(
+            value: value,
+            child: Text(value),
+          );
+        }).toList(),
+      ),
     );
   }
 }
