d1276e8cc6adb4b925eb595b4cddff6dcf98436c
Muhammad Rhomaedi <rhomaedycreator@gmail.com>
2024-02-26 21:33:29 +0700

    master: add willpopScope on page sdm

--- lib/src/futures/home/views/home_sdm/index.dart
+++ lib/src/futures/home/views/home_sdm/index.dart
@@ -9,6 +9,7 @@ import 'package:atendence_hcs/utils/components/colors.dart';
 import 'package:atendence_hcs/utils/components/space.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
 
 // ignore: must_be_immutable
@@ -36,98 +37,124 @@ class HomeSdm extends StatelessWidget {
     },
   ];
 
+  DateTime timeBackPresed = DateTime.now();
+
+  Future<bool> onPop() async {
+    final diference = DateTime.now().difference(timeBackPresed);
+    final isExitWaring = diference >= const Duration(seconds: 2);
+
+    timeBackPresed = DateTime.now();
+
+    if (isExitWaring) {
+      Fluttertoast.showToast(
+        msg: "Tekan Lagi untuk Keluar",
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1,
+        backgroundColor: const Color(0x8A272727),
+        textColor: Colors.black,
+      );
+      return false;
+    } else {
+      Fluttertoast.cancel();
+      return true;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark.copyWith(
       statusBarColor: Colors.black,
     ));
-    double heightStatusBar = MediaQuery.of(context).viewPadding.top;
-    return Scaffold(
-      primary: false,
-      backgroundColor: Colors.white,
-      body: NestedScrollView(
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-          return [
-            SliverOverlapAbsorber(
-              handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
-              sliver: Obx(
-                () => SliverAppBar(
-                  backgroundColor: cPrimary,
-                  foregroundColor: Colors.white,
-                  title: headerHomeSdm(
-                    prefsC.jenisKelamin.value,
-                    prefsC.namaKaryawan.value,
-                    prefsC.displayJabatan.value,
-                  ),
-                  actionsIconTheme: const IconThemeData(opacity: 0.0),
-                  flexibleSpace: FlexibleSpaceBar(
-                    background: Stack(
-                      children: <Widget>[
-                        Positioned.fill(
-                          child: Image.asset(
-                            "assets/images/header.png",
-                            fit: BoxFit.cover,
-                          ),
-                        )
-                      ],
+    return WillPopScope(
+      onWillPop: onPop,
+      child: Scaffold(
+        primary: false,
+        backgroundColor: Colors.white,
+        body: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return [
+              SliverOverlapAbsorber(
+                handle:
+                    NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+                sliver: Obx(
+                  () => SliverAppBar(
+                    backgroundColor: cPrimary,
+                    foregroundColor: Colors.white,
+                    title: headerHomeSdm(
+                      prefsC.jenisKelamin.value,
+                      prefsC.namaKaryawan.value,
+                      prefsC.displayJabatan.value,
                     ),
+                    actionsIconTheme: const IconThemeData(opacity: 0.0),
+                    flexibleSpace: FlexibleSpaceBar(
+                      background: Stack(
+                        children: <Widget>[
+                          Positioned.fill(
+                            child: Image.asset(
+                              "assets/images/header.png",
+                              fit: BoxFit.cover,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    titleTextStyle: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 17,
+                      fontWeight: FontWeight.w700,
+                    ),
+                    floating: false,
+                    pinned: true,
+                    toolbarHeight: 80,
+                    expandedHeight: 90,
+                    forceElevated: innerBoxIsScrolled,
                   ),
-                  titleTextStyle: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 17,
-                    fontWeight: FontWeight.w700,
-                  ),
-                  floating: false,
-                  pinned: true,
-                  toolbarHeight: 80,
-                  expandedHeight: 90,
-                  forceElevated: innerBoxIsScrolled,
                 ),
               ),
-            ),
-          ];
-        },
-        body: SafeArea(
-          child: Obx(
-            () => Padding(
-              padding: const EdgeInsets.only(top: 80),
-              child: RefreshIndicator(
-                onRefresh: () async {
-                  // Future.delayed(
-                  //   const Duration(seconds: 2),
-                  //   () =>
-                  // );
-                  homeSdmC.getDataHome();
-                },
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15),
-                  child: SingleChildScrollView(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        spaceHeight(20),
-                        homeSdmC.isLoading.value
-                            ? shimmerCardPayment()
-                            : cardPayment(homeSdmC.homeSdmM!.data.totalGaji),
-                        spaceHeight(25),
-                        listCardItems(listIcon),
-                        spaceHeight(25),
-                        Container(
-                          width: Get.width,
-                          height: 1,
-                          color: cGrey_400,
-                        ),
-                        spaceHeight(20),
-                        Text(
-                          "Rincian Data",
-                          style: customTextStyle(
-                              FontWeight.w600, 16, Colors.black),
-                        ),
-                        spaceHeight(20),
-                        homeSdmC.isLoading.value
-                            ? shimmerListRincianData()
-                            : listRincianData(homeSdmC.listRincian)
-                      ],
+            ];
+          },
+          body: SafeArea(
+            child: Obx(
+              () => Padding(
+                padding: const EdgeInsets.only(top: 80),
+                child: RefreshIndicator(
+                  onRefresh: () async {
+                    // Future.delayed(
+                    //   const Duration(seconds: 2),
+                    //   () =>
+                    // );
+                    homeSdmC.getDataHome();
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          spaceHeight(20),
+                          homeSdmC.isLoading.value
+                              ? shimmerCardPayment()
+                              : cardPayment(homeSdmC.homeSdmM!.data.totalGaji),
+                          spaceHeight(25),
+                          listCardItems(listIcon),
+                          spaceHeight(25),
+                          Container(
+                            width: Get.width,
+                            height: 1,
+                            color: cGrey_400,
+                          ),
+                          spaceHeight(20),
+                          Text(
+                            "Rincian Data",
+                            style: customTextStyle(
+                                FontWeight.w600, 16, Colors.black),
+                          ),
+                          spaceHeight(20),
+                          homeSdmC.isLoading.value
+                              ? shimmerListRincianData()
+                              : listRincianData(homeSdmC.listRincian)
+                        ],
+                      ),
                     ),
                   ),
                 ),
@@ -135,61 +162,61 @@ class HomeSdm extends StatelessWidget {
             ),
           ),
         ),
+        // body: RefreshIndicator(
+        //   onRefresh: () {
+        //     return Future.delayed(
+        //       const Duration(seconds: 2),
+        //       () => homeSdmC.getDataHome(),
+        //     );
+        //   },
+        //   backgroundColor: cPrimary,
+        //   color: Colors.white,
+        //   child: Padding(
+        //     padding: EdgeInsets.only(
+        //       top: heightStatusBar,
+        //       bottom: 10,
+        //       left: 20,
+        //       right: 20,
+        //     ),
+        //     child: Obx(
+        //       () => Column(
+        //         crossAxisAlignment: CrossAxisAlignment.start,
+        //         children: [
+        //           spaceHeight(25),
+        //           headerHomeSdm(
+        //             prefsC.jenisKelamin.value,
+        //             prefsC.namaKaryawan.value,
+        //             prefsC.displayJabatan.value,
+        //           ),
+        //           spaceHeight(25),
+        //           homeSdmC.isLoading.value
+        //               ? shimmerCardPayment()
+        //               : cardPayment(
+        //                   homeSdmC.homeSdmM!.data.totalGaji.replaceAll('.', ','),
+        //                 ),
+        //           spaceHeight(25),
+        //           listCardItems(listIcon),
+        //           spaceHeight(25),
+        //           Container(
+        //             width: Get.width,
+        //             height: 1,
+        //             color: cGrey_400,
+        //           ),
+        //           spaceHeight(20),
+        //           Text(
+        //             "Rincian Data",
+        //             style: customTextStyle(FontWeight.w600, 16, Colors.black),
+        //           ),
+        //           spaceHeight(20),
+        //           homeSdmC.isLoading.value
+        //               ? shimmerListRincianData()
+        //               : listRincianData(homeSdmC.listRincian)
+        //         ],
+        //       ),
+        //     ),
+        //   ),
+        // ),
       ),
-      // body: RefreshIndicator(
-      //   onRefresh: () {
-      //     return Future.delayed(
-      //       const Duration(seconds: 2),
-      //       () => homeSdmC.getDataHome(),
-      //     );
-      //   },
-      //   backgroundColor: cPrimary,
-      //   color: Colors.white,
-      //   child: Padding(
-      //     padding: EdgeInsets.only(
-      //       top: heightStatusBar,
-      //       bottom: 10,
-      //       left: 20,
-      //       right: 20,
-      //     ),
-      //     child: Obx(
-      //       () => Column(
-      //         crossAxisAlignment: CrossAxisAlignment.start,
-      //         children: [
-      //           spaceHeight(25),
-      //           headerHomeSdm(
-      //             prefsC.jenisKelamin.value,
-      //             prefsC.namaKaryawan.value,
-      //             prefsC.displayJabatan.value,
-      //           ),
-      //           spaceHeight(25),
-      //           homeSdmC.isLoading.value
-      //               ? shimmerCardPayment()
-      //               : cardPayment(
-      //                   homeSdmC.homeSdmM!.data.totalGaji.replaceAll('.', ','),
-      //                 ),
-      //           spaceHeight(25),
-      //           listCardItems(listIcon),
-      //           spaceHeight(25),
-      //           Container(
-      //             width: Get.width,
-      //             height: 1,
-      //             color: cGrey_400,
-      //           ),
-      //           spaceHeight(20),
-      //           Text(
-      //             "Rincian Data",
-      //             style: customTextStyle(FontWeight.w600, 16, Colors.black),
-      //           ),
-      //           spaceHeight(20),
-      //           homeSdmC.isLoading.value
-      //               ? shimmerListRincianData()
-      //               : listRincianData(homeSdmC.listRincian)
-      //         ],
-      //       ),
-      //     ),
-      //   ),
-      // ),
     );
   }
 }
