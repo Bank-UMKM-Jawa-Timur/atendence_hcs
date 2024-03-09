493299db162041b53352b6241eda9a1ad4c0ba04
Muhammad Rhomaedi <rhomaedycreator@gmail.com>
2024-03-09 16:38:52 +0700

    navigation-bar-sdm: fix menu laporan

--- lib/src/futures/SDM/menu/laporan.dart
+++ lib/src/futures/SDM/menu/laporan.dart
@@ -1,8 +1,11 @@
+import 'package:atendence_hcs/src/futures/home/views/home_sdm/components/modalButtomSheet/widgets/menu_laporan.dart';
+import 'package:atendence_hcs/src/futures/home/views/home_sdm/components/variable/list_menu.dart';
 import 'package:atendence_hcs/utils/components/all_widget.dart';
 import 'package:atendence_hcs/utils/components/colors.dart';
 import 'package:atendence_hcs/utils/components/my_appbar.dart';
 import 'package:atendence_hcs/utils/components/space.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class LaporanMenu extends StatefulWidget {
   const LaporanMenu({super.key});
@@ -12,6 +15,7 @@ class LaporanMenu extends StatefulWidget {
 }
 
 class _LaporanMenuState extends State<LaporanMenu> {
+  bool active = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,6 +31,141 @@ class _LaporanMenuState extends State<LaporanMenu> {
               style: textBoldDarkLarge,
             ),
             spaceHeight(10),
+            ListView.builder(
+              itemCount: ListMenuItems.menuLaporanList.length,
+              shrinkWrap: true,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 5),
+                  child: index == 0
+                      ? Container(
+                          width: Get.width,
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: cGrey_500,
+                              width: 1,
+                            ),
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(7),
+                            ),
+                          ),
+                          child: AnimatedSize(
+                            curve: Curves.linear,
+                            duration: const Duration(milliseconds: 300),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 15,
+                                vertical: 10,
+                              ),
+                              child: Column(
+                                children: [
+                                  InkWell(
+                                    splashColor: Colors.white,
+                                    onTap: () {
+                                      setState(() {
+                                        if (index == 0) {
+                                          active = !active;
+                                        }
+                                      });
+                                    },
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Row(
+                                          children: [
+                                            Icon(
+                                              ListMenuItems
+                                                      .menuLaporanList[index]
+                                                  ['icon'],
+                                              size: 20,
+                                              color: cPrimary,
+                                            ),
+                                            spaceWidth(10),
+                                            Text(
+                                              ListMenuItems
+                                                      .menuLaporanList[index]
+                                                  ['name'],
+                                              style: customTextStyle(
+                                                  FontWeight.w400,
+                                                  15,
+                                                  Colors.black),
+                                            ),
+                                          ],
+                                        ),
+                                        Icon(
+                                          active
+                                              ? Icons.arrow_drop_up_sharp
+                                              : Icons.arrow_drop_down_sharp,
+                                          size: 30,
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                  index == 0
+                                      ? active
+                                          ? MenuLaporan.subMenu()
+                                          : Container()
+                                      : Container(),
+                                ],
+                              ),
+                            ),
+                          ),
+                        )
+                      : InkWell(
+                          onTap: () {
+                            Get.back();
+                            Get.toNamed(
+                              ListMenuItems.menuLaporanList[index]['route'],
+                              // arguments: [
+                              //   {
+                              //     'nip': '',
+                              //   },
+                              //   {
+                              //     'nama': 'Cari Karyawan',
+                              //   },
+                              // ],
+                            );
+                          },
+                          child: Container(
+                            width: Get.width,
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: cGrey_500,
+                                width: 1,
+                              ),
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(7),
+                              ),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 15,
+                                vertical: 15,
+                              ),
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    ListMenuItems.menuLaporanList[index]
+                                        ['icon'],
+                                    size: 20,
+                                    color: cPrimary,
+                                  ),
+                                  spaceWidth(10),
+                                  Text(
+                                    ListMenuItems.menuLaporanList[index]
+                                        ['name'],
+                                    style: customTextStyle(
+                                        FontWeight.w400, 15, Colors.black),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                );
+              },
+            ),
           ],
         ),
       ),
