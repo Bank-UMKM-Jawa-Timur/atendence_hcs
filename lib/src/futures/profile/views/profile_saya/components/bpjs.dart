2a5ab29ee0b01f9dca15fc9889e6edea8012c13b
Muhammad Rhomaedi <rhomaedycreator@gmail.com>
2024-04-13 21:17:10 +0700

    master: fix bpjs & ketenaga kerjaan

--- lib/src/futures/profile/views/profile_saya/components/bpjs.dart
+++ lib/src/futures/profile/views/profile_saya/components/bpjs.dart
@@ -1,6 +1,12 @@
 import 'package:atendence_hcs/utils/components/colors.dart';
+import 'package:atendence_hcs/utils/components/space.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+List bpjsKetenagakerjaan = [
+  'Kartu Peserta Jamsostek (KPJ)',
+  'aminan Kesehatan Nasional (JKN)',
+];
 
 Widget bpjsKetenagaKerjaan() {
   return Expanded(
@@ -13,9 +19,53 @@ Widget bpjsKetenagaKerjaan() {
           color: cWhite,
           borderRadius: BorderRadius.circular(10),
         ),
-        child: const Padding(
-          padding: EdgeInsets.all(10),
-          child: Text("BPJS"),
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Expanded(
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: bpjsKetenagakerjaan.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 5),
+                  child: Container(
+                    width: Get.width,
+                    padding: const EdgeInsets.symmetric(vertical: 4),
+                    decoration: const BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(
+                          color: cGrey_400,
+                          width: 1,
+                        ),
+                      ),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          bpjsKetenagakerjaan[index],
+                          style: const TextStyle(
+                            fontSize: 13,
+                            color: cGrey_700,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        spaceHeight(7),
+                        Text(
+                          '-',
+                          style: const TextStyle(
+                            fontSize: 14,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
         ),
       ),
     ),
