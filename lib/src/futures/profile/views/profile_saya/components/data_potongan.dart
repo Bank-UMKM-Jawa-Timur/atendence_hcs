2ea361a47ef40605d0de2cca1bbb7dab7803684f
Muhammad Rhomaedi <rhomaedycreator@gmail.com>
2024-04-13 21:31:11 +0700

    master: fix data potongan

--- lib/src/futures/profile/views/profile_saya/components/data_potongan.dart
+++ lib/src/futures/profile/views/profile_saya/components/data_potongan.dart
@@ -1,6 +1,12 @@
 import 'package:atendence_hcs/utils/components/colors.dart';
+import 'package:atendence_hcs/utils/components/space.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+List dataPotonganList = [
+  "Iuran IK",
+  "Total Potongan",
+];
 
 Widget dataPotongan() {
   return Expanded(
@@ -13,9 +19,53 @@ Widget dataPotongan() {
           color: cWhite,
           borderRadius: BorderRadius.circular(10),
         ),
-        child: const Padding(
-          padding: EdgeInsets.all(10),
-          child: Text("DATA POTONGAN"),
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Expanded(
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: dataPotonganList.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 5),
+                  child: Container(
+                    width: Get.width,
+                    padding: const EdgeInsets.symmetric(vertical: 4),
+                    decoration: const BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(
+                          color: cGrey_400,
+                          width: 1,
+                        ),
+                      ),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "${dataPotonganList[index]} :",
+                          style: const TextStyle(
+                            fontSize: 13,
+                            color: cGrey_700,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        spaceHeight(7),
+                        Text(
+                          '-',
+                          style: const TextStyle(
+                            fontSize: 14,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
         ),
       ),
     ),
